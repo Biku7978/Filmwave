3984938ada78e886ada4115432fbd2cdfc0ca0ff
Biku7978 <bikrambariki143@gmail.com>
2026-07-21 17:43:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-fetch('movies.json')
-    .then(response => response.json())
-    .then(data => {
-        const grid = document.getElementById('movieGrid');
-        data.forEach(movie => {
-            grid.innerHTML += `
-                <div class="movie-card">
-                    <img src="${movie.poster}" width="100">
-                    <h3>${movie.title}</h3>
-                    <a href="details.html?id=${movie.id}">Watch Now</a>
-                </div>
+const API_KEY = "YOUR_API_KEY";
+
+async function searchMovie(){
+
+    let name = document.getElementById("search").value;
+
+    let url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${name}`;
+
+    let response = await fetch(url);
+    let data = await response.json();
+
+    let movies = document.getElementById("movies");
+
+    movies.innerHTML = "";
+
+    if(data.Search){
+
+        data.Search.forEach(movie => {
+
+            movies.innerHTML += `
+            <div class="movie">
+                <img src="${movie.Poster}">
+                <h3>${movie.Title}</h3>
+                <p>${movie.Year}</p>
+            </div>
             `;
+
         });
-    });
+
+    }else{
+        movies.innerHTML = "<h2>Movie Not Found</h2>";
+    }
+}
