280263c3e8ff08545864b7f636a258a1ff207355
Biku7978 <bikrambariki143@gmail.com>
2026-07-21 19:29:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
-async function loadShows(){
+const movies = [
+{
+title:"Jawan",
+year:"2023",
+category:"Bollywood",
+image:"https://via.placeholder.com/300x450?text=Jawan"
+},
+{
+title:"Pathaan",
+year:"2023",
+category:"Bollywood",
+image:"https://via.placeholder.com/300x450?text=Pathaan"
+},
+{
+title:"Avengers Endgame",
+year:"2019",
+category:"Hollywood",
+image:"https://via.placeholder.com/300x450?text=Avengers"
+},
+{
+title:"Spider Man",
+year:"2021",
+category:"Hollywood",
+image:"https://via.placeholder.com/300x450?text=Spider+Man"
+},
+{
+title:"Indian TV Serial",
+year:"2026",
+category:"TV Shows",
+image:"https://via.placeholder.com/300x450?text=TV+Show"
+}
+];
 
-const response = await fetch("https://api.tvmaze.com/shows");
-const shows = await response.json();
 
 let box = document.getElementById("movies");
 
-box.innerHTML = "";
 
-shows.slice(0,20).forEach(show=>{
+function showMovies(list){
+
+box.innerHTML="";
+
+list.forEach(movie=>{
 
 box.innerHTML += `
 <div class="movie">
 
-<img src="${show.image ? show.image.medium : 'https://via.placeholder.com/300x450'}">
+<img src="${movie.image}">
 
-<h3>${show.name}</h3>
+<h3>${movie.title}</h3>
 
-<p>⭐ ${show.rating.average || "N/A"}</p>
+<p>${movie.year}</p>
+
+<p>${movie.category}</p>
 
 </div>
 `;
@@ -25,4 +59,18 @@ box.innerHTML += `
 
 }
 
-loadShows();
+
+showMovies(movies);
+
+
+function searchMovie(){
+
+let text=document.getElementById("search").value.toLowerCase();
+
+let result=movies.filter(movie =>
+movie.title.toLowerCase().includes(text)
+);
+
+showMovies(result);
+
+}
