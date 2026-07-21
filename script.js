f9e5b3db34d66404329924e0b43bb6b90197c5aa
Biku7978 <bikrambariki143@gmail.com>
2026-07-21 18:58:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,27 @@
-const movies = [
-{
-title:"Avengers Endgame",
-year:"2019",
-image:"https://m.media-amazon.com/images/I/81ExhpBEbHL.jpg"
-},
-{
-title:"KGF Chapter 2",
-year:"2022",
-image:"https://m.media-amazon.com/images/I/71QZ8z9wKRL.jpg"
-},
-{
-title:"RRR",
-year:"2022",
-image:"https://m.media-amazon.com/images/I/81lE8J5YQ-L.jpg"
-}
-];
+const API_KEY = "wm_zOtt5ZItPRmx7G1EcoMd9tNIkq4HOC-6OPNwgbJ7sfc"
 
-let box = document.getElementById("movies");
+async function loadMovies(){
 
-function showMovies(list){
+    const url = `https://api.watchmode.com/v1/list-titles/?apiKey=${API_KEY}&types=movie&limit=10`;
 
-box.innerHTML="";
+    const response = await fetch(url);
+    const data = await response.json();
 
-list.forEach(movie=>{
+    let box = document.getElementById("movies");
+    box.innerHTML = "";
 
-box.innerHTML += `
-<div class="movie" onclick="openMovie()">
-<img src="${movie.image}">
-<h3>${movie.title}</h3>
-<p>${movie.year}</p>
-</div>
-`;
+    data.titles.forEach(movie => {
 
-});
+        box.innerHTML += `
+        <div class="movie">
+            <img src="${movie.poster}">
+            <h3>${movie.title}</h3>
+            <p>${movie.year}</p>
+        </div>
+        `;
+
+    });
 
 }
 
-showMovies(movies);
-
-
-function searchMovie(){
-
-let text = document.getElementById("search").value.toLowerCase();
-
-let result = movies.filter(movie =>
-movie.title.toLowerCase().includes(text)
-);
-
-showMovies(result);
-
-}
-
-
-function openMovie(){
-window.location.href="movie.html";
-}
+loadMovies();
